60090a7476a1f6c531017729f3f7b7d9ecd6ebfb
bipoza <bipoza@gmail.com>
2023-03-21 12:52:21 +0100

    Add admin menu panel

--- wp-uptime.php
+++ wp-uptime.php
@@ -19,11 +19,19 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+function get_option_or_value($key, $default_value)
+{
+    $response = get_option($key);
+    if (!$response) {
+        $response = $default_value;
+    }
+    return $response;
+}
 
 add_action('rest_api_init', function () {
     register_rest_route(
         'wp-uptime',
-        '/ok/',
+        '/' . get_option_or_value("endpoint_path", 'ok') . '/',
         array(
             'methods' => 'GET',
             'callback' => 'wp_uptime_route_handler',
@@ -31,11 +39,61 @@ add_action('rest_api_init', function () {
     );
 });
 
+
 function wp_uptime_route_handler($request)
 {
     global $wpdb;
     $result = $wpdb->check_connection();
+
+
     if ($result) {
-        echo 'OK';
+        echo get_option_or_value("response_value", 'OK');
     }
+}
+
+add_action('admin_menu', 'wp_uptime_menu');
+function wp_uptime_menu()
+{
+    add_menu_page('WP Uptime Settings', 'WP Uptime', 'manage_options', 'wp-uptime-settings', 'wp_uptime_settings_page');
+}
+
+function wp_uptime_settings_page()
+{
+    $url = get_rest_url(null, 'wp-uptime/' . get_option_or_value("endpoint_path", 'ok') . '/');
+
+
+    ?>
+    <div class="wrap">
+        <h1>WP Uptime Settings</h1>
+        <h3>
+            Editable non-cached monitorization path for Wordpress
+        </h3>
+        <p>Monitorization path: <a href="<?php echo $url ?>" target="_blank"><?php echo $url ?></a></p>
+        <form method="post" action="options.php">
+            <?php settings_fields('wp_uptime_settings'); ?>
+            <?php do_settings_sections('wp-uptime-settings'); ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Endpoint path</th>
+                    <td><input type="text" name="endpoint_path"
+                            value="<?php echo esc_attr(get_option_or_value('endpoint_path', 'ok')); ?>"></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">OK response value</th>
+                    <td><input type="text" name="response_value"
+                            value="<?php echo esc_attr(get_option_or_value('response_value', 'OK')); ?>"></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+add_action('admin_init', 'wp_uptime_settings');
+
+function wp_uptime_settings()
+{
+    register_setting('wp_uptime_settings', 'endpoint_path');
+    register_setting('wp_uptime_settings', 'response_value');
 }
